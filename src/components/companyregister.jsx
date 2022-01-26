5d7d93195d96bd25c190f2564e8b9aee5dbdc331
Shreyas <rasaikarshreyas21@gmail.com>
2022-01-26 22:41:33 +0530

    Updated CompanyRegister and MainRegister

--- src/components/companyregister.jsx
+++ src/components/companyregister.jsx
@@ -1,53 +1,64 @@
 import React from "react";
 
-function CompanyRegister() {
+const CompanyRegister = () => {
   return (
     <>
-      <div className="container flex items-center justify-center flex-col gap-5">
-        <h1>Register Your Company Now!</h1>
+      <div className="min-h-screen flex items-center justify-center bg-blue-400">
+        <div className="bg-white p-16 rounded shadow-2xl w-auto">
+          <h1 className="text-2xl font-bold mb-4">
+            Register your Company Now!
+          </h1>
 
-        <form className="space-y-6">
-          <div>
-            <label className="block">Company Name</label>
-            <input type="text" />
-          </div>
+          <form className="space-y-6">
+            <div className="flex">
+              <div>
+                <div>
+                  <label className="block">Company Name</label>
+                  <input className="mt-2 rounded" type="text" />
+                </div>
 
-          <div>
-            <label className="block"> Email</label>
-            <input type="email" />
-          </div>
+                <div className="mt-3">
+                  <label className="block">Email</label>
+                  <input className="mt-2 rounded" type="email" />
+                </div>
 
-          <div>
-            <label className="block">CIN Number</label>
-            <input type="number" required />
-          </div>
+                <div className="mt-3">
+                  <label className="block">Pan Card</label>
+                  <input className="mt-2 block" type="file" />
+                </div>
 
-          <div>
-            <label className="block">Blank cheque</label>
-            <input type="file" required />
-          </div>
+                <div className="mt-3">
+                  <label className="block">Blank Cheque</label>
+                  <input className="mt-2" type="file" />
+                </div>
+              </div>
 
-          <div>
-            <label className="block">PAN card</label>
-            <input type="file" required />
-          </div>
+              <div>
+                <div>
+                  <label className="block">CIN Number</label>
+                  <input className="mt-2 rounded" type="number" />
+                </div>
 
-          <div>
-            <label className="block">City</label>
-            <input type="text" />
-          </div>
+                <div className="mt-3">
+                  <label className="block">City</label>
+                  <input className="mt-2 rounded" type="text" />
+                </div>
 
-          <div>
-            <label className="block">Postal Code</label>
-            <input type="number" />
-          </div>
-          <button className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            Sign Up
-          </button>
-        </form>
+                <div className="mt-3">
+                  <label className="block">Postal Code</label>
+                  <input className="mt-2 rounded" type="number" />
+                </div>
+              </div>
+            </div>
+
+            <button className="ml-[155px] w-1/3 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Sign Up
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
-}
+};
 
 export default CompanyRegister;
