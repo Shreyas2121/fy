e30f522b7acbb3b250638f13cb65825b4fa6f133
Shreyas <rasaikarshreyas21@gmail.com>
2022-04-08 20:37:40 +0530

    8.4.3

--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,6 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
